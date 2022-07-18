573fbafaade61e1e704467817e55defca508b845
alextm <69592636+alextm0@users.noreply.github.com>
2022-07-18 23:07:23 +0300

    inca putin de lucrat la mobil

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,22 +6,33 @@ import HeroImage from '../assets/hero-illustration.png'
 function Hero() {
   return (
     <div className='text-white'>
-      <div className='flex items-center justify-evenly'>
-        <div className='max-w-[500px] xl:max-w-[700px] pl-10 md:pl-14     mt-[-90px] h-screen flex flex-col justify-center'>
-          <p className='text-[42px] xl:text-[45px] 2xl:text-[48px]      drop-shadow-[0px_4px_4px_rgba(0,0,0,0.30)] leading-[120%] font-righteous'>
+
+      {/* whole content of */}
+      <div className='flex flex-col sm:flex-row items-center justify-evenly'>
+
+        {/* Header content container */}
+        <div className='w-full sm:max-w-[500px] xl:max-w-[700px] pl-5 sm:pl-10 md:pl-14 sm:h-screen sm:mt-[-0px] flex flex-col justify-center'>
+          {/* Main header text */}
+          <p className='text-[32px] xl:text-[45px] 2xl:text-[48px]      drop-shadow-[0px_4px_4px_rgba(0,0,0,0.30)] leading-[120%] font-righteous'>
             Invata informatica <br /> si atinge-ti scopurile
           </p>
 
+          {/* Description text */}
           <p className='text-[16px] 2xl:text-[18px]      font-thin font-quicksand mt-[30px] w-[90%]'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sodales diam sed rhoncus enim, sed aliquet ac quam. Aliquet pellentesque bibendum tellus in tellus. </p>
 
-          <button className='w-[130px] h-[45px] text-[14px] xl:text-[16px] xl:w-[150px] xl:h-[50px]      bg-[#EC615B] rounded-lg font-quicksand font-bold flex justify-center items-center gap-2 mt-[62px]'>
+          {/* CTA Button */}
+          <button className='w-[130px] h-[45px] text-[14px] xl:text-[16px] xl:w-[150px] xl:h-[50px]      bg-[#EC615B] rounded-lg font-quicksand font-bold flex justify-center items-center gap-2 mt-[32px] sm:mt-[62px]'>
             Incepe aici <BsArrowRight className='text-lg' />
           </button>
         </div>
+
+        {/* Illustration */}
         <div className=''>
-          <img className='mb-20 w-[513px] h-[401px] xl:w-[613px] xl:h-[501px]' src={HeroImage} alt="hero-illustration" />
+          <img className='sm:mb-20 w-[343px] h-[291px] xl:w-[613px] xl:h-[501px]' src={HeroImage} alt="hero-illustration" />
         </div>
+
       </div>
+
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,23 @@
 import React from 'react'
 
 import Logo from '../assets/logo.png'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 function Navbar() {
   return (
-    <div className=' mx-auto h-[90px]  bg-cover flex flex-col justify-center text-white'>
+    <div className='mx-auto h-[90px] flex flex-col justify-center text-white'>
       <div className='w-full flex items-center justify-between px-8 md:px-16 '>
         <div className='flex items-center gap-2'>
           <img className='w-[45px] h-[45px]' src={Logo} alt="logo" />
           <span className='text-2xl md:text-3xl font-righteous '> CodeWiki </span>
         </div>
 
-        <ul className='flex gap-8 md:gap-16 text-xl md:text-2xl font-quicksand'>
+        <ul className='hidden sm:flex gap-8 md:gap-16 text-xl md:text-2xl font-quicksand'>
           <li> Articole </li>
           <li> Categorii </li>
           <li> Probleme </li>
         </ul>
+        <GiHamburgerMenu className='sm:hidden w-7 h-7' />
       </div>
     </div>
   )
